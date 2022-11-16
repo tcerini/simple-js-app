8fc62d131f4b062cd33ead967099180e53e68b56
tcerini <tommycerini@gmail.com>
2022-11-16 16:00:23 +0100

    added pokemon list array

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,8 @@
-alert('Hello world');
-let favouriteFood = 'Pizza';
-document.write(favouriteFood);
+let pokemonList = [
+  { name: 'Bulbasaur', height: 0.7, type: ['grass', 'poison']},
+  { name: 'Mr. Mime', height: 1.3, type: ['psychic', 'fairy']},
+  { name: 'Sandshrew', height: 0.6, type: ['ground']},
+  { name: 'Haunter', height: 1.6, type: ['ghost', 'poison']},
+  { name: 'Tentacruel', height: 1.6, type: ['water', 'poison']},
+  { name: 'Arbok', height: 3.5, type: ['poison']},
+]
