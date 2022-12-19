759ac405c0004fa396b21212da092c945f0fa661
tcerini <tommycerini@gmail.com>
2022-12-20 07:21:53 +1100

    update repository adds to individual objects

--- js/scripts.js
+++ js/scripts.js
@@ -40,14 +40,19 @@ let pokemonRepository = function () {
 
 //adding Pokemon to IFFE pokemonList
 pokemonRepository.add(
-{ name: 'Bulbasaur', height: 0.7, type: ['grass', 'poison']},
-{ name: 'Mr. Mime', height: 1.3, type: ['psychic', 'fairy']},
-{ name: 'Sandshrew', height: 0.6, type: ['ground']},
-{ name: 'Haunter', height: 1.6, type: ['ghost', 'poison']},
-{ name: 'Tentacruel', height: 1.6, type: ['water', 'poison']},
+{ name: 'Bulbasaur', height: 0.7, type: ['grass', 'poison']});
+pokemonRepository.add(
+{ name: 'Mr. Mime', height: 1.3, type: ['psychic', 'fairy']});
+pokemonRepository.add(
+{ name: 'Sandshrew', height: 0.6, type: ['ground']});
+pokemonRepository.add(
+{ name: 'Haunter', height: 1.6, type: ['ghost', 'poison']});
+pokemonRepository.add(
+{ name: 'Tentacruel', height: 1.6, type: ['water', 'poison']});
+pokemonRepository.add(
 { name: 'Arbok', height: 3.5, type: ['poison']});
 
 //forEach loop to iterate over pokemon array
 pokemonList.forEach(function(pokemon) {
-  document.write(pokemon.name + ' is ' + pokemon.height + 'm tall' + ' and is a ' + pokemon.type + ' type Pokemon');
+  console.log(pokemon.name + ' is ' + pokemon.height + 'm tall' + ' and is a ' + pokemon.type + ' type Pokemon');
 });
