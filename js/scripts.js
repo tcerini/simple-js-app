4740ca21a10323f5fc9d04301656be3820bf8799
tcerini <tommycerini@gmail.com>
2022-12-21 05:16:26 +1100

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,25 +1,3 @@
-//OLD CODE START
-//Pokemon list in array
-// let pokemonList = [
-//   { name: 'Bulbasaur', height: 0.7, type: ['grass', 'poison']},
-//   { name: 'Mr. Mime', height: 1.3, type: ['psychic', 'fairy']},
-//   { name: 'Sandshrew', height: 0.6, type: ['ground']},
-//   { name: 'Haunter', height: 1.6, type: ['ghost', 'poison']},
-//   { name: 'Tentacruel', height: 1.6, type: ['water', 'poison']},
-//   { name: 'Arbok', height: 3.5, type: ['poison']},
-// ]
-//
-// //Writing all items from pokemonList array with callout text for largest pokemon
-// for (let i = 0; i < pokemonList.length; i++) {
-//   if (pokemonList[i].height > 3.4) {
-//     document.write(pokemonList[i].name + " (Height: " + pokemonList[i].height + ")" + " - Wow. That's Big!<br>")
-//   }
-//   else {
-//     document.write( pokemonList[i].name + " (Height: " + pokemonList[i].height + ")<br>")
-//   }
-// }
-// OLD CODE END
-
 //IIFE
 let pokemonRepository = function () {
   let pokemonList = [];
