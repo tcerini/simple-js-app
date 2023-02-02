599a6b4953e0a0d923c79d086a7c56395065b667
tcerini <tommycerini@gmail.com>
2023-02-02 15:19:53 +0100

    attempts to add list of pokemon with button

--- js/scripts.js
+++ js/scripts.js
@@ -18,7 +18,7 @@ let pokemonRepository = function () {
 
 let pokemonList = pokemonRepository.getAll();
 
-//adding Pokemon to IFFE pokemonList
+//adding Pokemon to IIFE pokemonList
 pokemonRepository.add(
 { name: 'Bulbasaur', height: 0.7, type: ['grass', 'poison']});
 pokemonRepository.add(
@@ -34,5 +34,27 @@ pokemonRepository.add(
 
 //forEach loop to iterate over pokemon array
 pokemonList.forEach(function(pokemon) {
-  document.write( pokemon.name + ' is ' + pokemon.height + 'm tall' + ' and is a ' + pokemon.type + ' type Pokemon<br>');
+  let pokemonList = document.querySelector(".pokemon-list")
+  let listPokemon = document.createElement('.li')
+  let button = document.createElement('button')
+  button.innerText = pokemon.name;
+  button.classList.add("pokemon-button")
+  listPokemon.appendChild(button);
+  pokemonList.appendChild(listPokemon)
 });
+
+// function addListItem (pokemon) {
+//   let pokemonList = document.querySelector(".pokemon-list")
+//   let listPokemon = document.createElement('.li')
+//   let button = document.createElement('button')
+//   button.innerText = pokemon.name;
+//   button.classList.add("pokemon-button")
+//   listPokemon.appendChild(button);
+//   pokemonList.appendChild(listPokemon)
+// }
+
+
+//Old for each code
+//pokemonList.forEach(function(pokemon) {
+//  document.write( pokemon.name + ' is ' + pokemon.height + 'm tall' + ' and is a ' + pokemon.type + ' type Pokemon<br>');
+//});
